cecbcebc4ea0037cde26f240c57029cda3e313c6
Don Aldrin Beringuela <drinberinguela@gmail.com>
2022-08-17 00:42:55 +0800

    Pass Activity 02 (repeatString function)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  let strRepeated = str;
+
+  if (num < 0) {
+    return 'ERROR';
+  } else if( (num == 0) || (num == '') ) {
+    return '';
+  } else {
+    while(num > 1) {
+      strRepeated += str;
+      num--;
+    }
+    return strRepeated; 
+  }
 
 };
 
