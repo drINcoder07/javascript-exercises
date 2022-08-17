fddcf0e94b6e4268a6a5addfc6d6dabae7b98841
Don Aldrin Beringuela <drinberinguela@gmail.com>
2022-08-17 23:23:58 +0800

    Pass Activity 04 (removeFromArray)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+  //iterate over value in values
+  //check whether value in arr
+  //remove value in arr (or store in another array maybe)
+  //loop again to next value
+  //return new arr
 
+  for (value of values) {
+    if (arr.indexOf(value) == -1) {
+      continue;
+    } else {
+      arr.splice(arr.indexOf(value), 1)
+    }
+  }
+
+  return arr;
+  
 };
 
 // Do not edit below this line
