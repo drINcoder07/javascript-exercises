d38048fdc74d73f10ff72a940d7547ddba45b9fa
Don Aldrin Beringuela <drinberinguela@gmail.com>
2022-08-17 09:04:13 +0800

    Pass Activity 03 (reverseString function)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  /* ONE LINE
+  return str.split('').reverse().join(''); */
 
+  // convert str to array
+  let strArray = str.split("");
+  let strReversed = "";
+  // get last letter out
+  for (i = str.length - 1; i >= 0 ; i--) {
+    strReversed += strArray.pop()
+  }
+  // push to new string
+  return strReversed;
 };
 
 // Do not edit below this line
