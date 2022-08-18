d0178a2c99cecc2186a73d13a1696226a4ef3c5b
Don Aldrin Beringuela <drinberinguela@gmail.com>
2022-08-18 10:25:08 +0800

    Pass Activity 05 (sumAll)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sumOfNumbers;
+  let largerNum, smallerNum;
+
+  if (typeof(num1) == 'string' || typeof(num2) == 'string' || num1 < 0 || num2 < 0 ) {
+    return ' ERROR';
+  } else {
+
+    if (num1 > num2) {
+      largerNum = num1;
+      smallerNum = num2;
+    } else {
+      largerNum = num2;
+      smallerNum = num1;
+    }
+
+    for ( let i = smallerNum; i <= largerNum; i++)  {
+      sumOfNumbers += i
+    }
+
+    return sumOfNumbers;
+  }
 
 };
 
