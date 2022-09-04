90a08c406f9799d775d43aa97b604292d4be31e2
Don Aldrin Beringuela <drinberinguela@gmail.com>
2022-09-04 18:06:05 +0800

    Pass Activity 08 (calculator app)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(...args) {
+  let result = 0;
+  for (let i = 0; i < args.length; i++) {
+    result+= arguments[i];
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	let result = args[0];
+  for (let i = 1; i < args.length; i++) {
+    result-= arguments[i];
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result+= arr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    result*= arr[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let result = 1;
+	for (let i=0; i < exp; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  let nums = num;
+  if (nums == 0) {
+    return result;
+  } else {
+    for (let i = 0; i < num; i++) {
+      result*= nums;
+      nums--;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
