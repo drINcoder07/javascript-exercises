de0889ea36f4b29285cd785d602634bf948e78f1
Don Aldrin Beringuela <drinberinguela@gmail.com>
2022-09-06 01:35:38 +0800

    Pass Activity 09 (palindromes)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let newStr = str.toLowerCase().replace(/[^\w]/g, "");
+  let arr = newStr.split('');
+  let temp = [...arr]
+  let arrReverse = temp.reverse();
+  let result = true;
 
+  for (let i=0; i < arr.length; i++) {
+    if (arr[i] != arrReverse[i]) {
+      result = false;
+      break;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
